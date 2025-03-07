ff5df40ecfa67fbcce72f6e69d8f97b526bd1756
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-03-07 21:20:19 +0800

    Update uninstall_filebrowser.sh

--- bash/uninstall_filebrowser.sh
+++ bash/uninstall_filebrowser.sh
@@ -4,6 +4,7 @@ TARGET_DIR="/filebrowser"
 CONFIG_DB="/etc/filebrowser.db"
 LOG_FILE="/var/log/filebrowser.log"
 RC_LOCAL="/etc/rc.local"
+SHARE_FILES="/filebrowsersharefiles"
 
 echo "正在停止 FileBrowser 进程..."
 pkill -f "$TARGET_DIR/filebrowser"
@@ -12,6 +13,7 @@ echo "正在删除 FileBrowser 相关文件..."
 rm -rf "$TARGET_DIR"
 rm -f "$CONFIG_DB"
 rm -f "$LOG_FILE"
+rm -f "$SHARE_FILES"
 
 if [ -f "$RC_LOCAL" ]; then
     sed -i '/filebrowser/d' "$RC_LOCAL"
