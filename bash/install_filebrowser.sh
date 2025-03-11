f0665393c9c6827a55fa523e7a04dedded148d80
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-03-11 18:05:56 +0800

    Update install_filebrowser.sh

--- bash/install_filebrowser.sh
+++ bash/install_filebrowser.sh
@@ -23,32 +23,22 @@ if [ -d "$TARGET_DIR" ]; then
     echo "检测到 $TARGET_DIR 存在，正在删除..."
     rm -rf "$TARGET_DIR"
 fi
-
 mkdir -p "$TARGET_DIR"
-
 LATEST_RELEASE=$(curl -sL https://api.github.com/repos/filebrowser/filebrowser/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-
 FILE_NAME="linux-amd64-filebrowser.tar.gz"
 FILEBROWSER_URL="https://github.com/filebrowser/filebrowser/releases/download/$LATEST_RELEASE/$FILE_NAME"
-
 curl -L "$FILEBROWSER_URL" -o "$TARGET_DIR/$FILE_NAME"
-
 if [ ! -f "$TARGET_DIR/$FILE_NAME" ]; then
     echo "下载失败，请检查网络连接或 GitHub 资源是否可用！"
     exit 1
 fi
-
 tar -xzvf "$TARGET_DIR/$FILE_NAME" -C "$TARGET_DIR"
-
 rm -f "$TARGET_DIR/$FILE_NAME"
-
 if [ ! -f "$TARGET_DIR/filebrowser" ]; then
     echo "解压失败，未找到 filebrowser 可执行文件！"
     exit 1
 fi
-
 echo "FileBrowser ($LATEST_RELEASE) 已下载并解压到 /$TARGET_DIR，接下来进行配置修改"
-
 chmod +x "$TARGET_DIR"/filebrowser
 
 "$TARGET_DIR"/filebrowser -d $CONFIG_DB config init >/dev/null 2>&1
@@ -61,11 +51,8 @@ chmod +x "$TARGET_DIR"/filebrowser
 "$TARGET_DIR"/filebrowser -d $CONFIG_DB users add $USERNAME admin --perm.admin >/dev/null 2>&1
 
 nohup "$TARGET_DIR"/filebrowser -d $CONFIG_DB >/dev/null 2>&1 &
-
 PID=$!
-
 sleep 2
-
 if ps -p $PID > /dev/null; then
     sed -i '/exit 0/i\nohup filebrowser -d \/etc\/filebrowser.db >\/dev\/null 2>&1 &' /etc/rc.local
     echo
@@ -87,4 +74,3 @@ else
     echo "filebrowser 启动失败，已删除相关文件"
     echo
 fi
-
