1b6daa47771aadc3ebdcaa57f6f156f2ce650739
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-04-09 20:12:55 +0800

    Update install_filebrowser.sh

--- bash/install_filebrowser.sh
+++ bash/install_filebrowser.sh
@@ -75,7 +75,7 @@ fi
 "$TARGET_DIR"/filebrowser -d $CONFIG_DB config set --log $LOG_FILE >/dev/null 2>&1
 "$TARGET_DIR"/filebrowser -d $CONFIG_DB config set --baseurl /files >/dev/null 2>&1
 "$TARGET_DIR"/filebrowser -d $CONFIG_DB config set --root $SHARE_FILES >/dev/null 2>&1
-"$TARGET_DIR"/filebrowser -d $CONFIG_DB users add $USERNAME admin --perm.admin >/dev/null 2>&1
+"$TARGET_DIR"/filebrowser -d $CONFIG_DB users add $USERNAME $USERNAME --perm.admin >/dev/null 2>&1
 
 echo "正在创建 systemd 服务文件..."
 
